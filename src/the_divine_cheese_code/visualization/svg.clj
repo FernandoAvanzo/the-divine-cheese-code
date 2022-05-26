05746e88be8b2965df9fe5061a68ecb02a231560
fernando.avanzo <fernando.avanzo@gmail.com>
2022-05-26 18:18:22 -0300

    the-divine-cheese:sample-code: function catch a burgler

--- src/the_divine_cheese_code/visualization/svg.clj
+++ src/the_divine_cheese_code/visualization/svg.clj
@@ -14,10 +14,10 @@
 (defn comparator-over-maps
   [comparison-fn ks]
   (fn [maps]
-    (zipmap
-      ks (map
-           (fn [k]
-             (apply comparison-fn (map k maps)))))))
+    (zipmap ks
+            (map (fn [k] (apply comparison-fn (map k maps)))
+                 ks))))
+
 
 (def min (comparator-over-maps clojure.core/min [:lat :lng]))
 (def max (comparator-over-maps clojure.core/max [:lat :lng]))
