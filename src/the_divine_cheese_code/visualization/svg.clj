536714560e8ef9b63833b0112730b9c1eee58723
fernando.avanzo <fernando.avanzo@gmail.com>
2022-05-30 12:18:09 -0300

    the-divine-cheese:sample-code: functions to draw the lines is done

--- src/the_divine_cheese_code/visualization/svg.clj
+++ src/the_divine_cheese_code/visualization/svg.clj
@@ -1,6 +1,6 @@
 (ns the-divine-cheese-code.visualization.svg
   (:refer-clojure :exclude [max min])
-  (:require [clojure.string]))
+  (:require [clojure.string :as s]))
 
 (defn latlng->point
   "Convert lat/lng map to comma-separated string"
@@ -8,8 +8,13 @@
   (str (:lng latlng) " , " (:lat latlng)))
 
 (defn points
+  "Given a seq of lat/lng maps, return string of points joined by space"
   [locations]
-  (clojure.string/join " " (map latlng->point locations)))
+  (s/join " " (map latlng->point locations)))
+
+(defn line
+  [points]
+  (str "<polyline points=\"" points "\" />"))
 
 (defn comparator-over-maps
   [comparison-fn ks]
@@ -33,3 +38,23 @@
         ratio {:lat (/ height (:lat maxcoords))
                :lng (/ width (:lng maxcoords))}]
     (map #(merge-with * % ratio) locations)))
+
+(defn transform
+  "Just chains other functions"
+  [width height locations]
+  (->> locations
+       translate-to-00
+       (scale width height)))
+
+(defn xml
+  "svg 'template', which also flips the coordinate system"
+  [width height locations]
+  (str "<svg height=\"" height "\" width=\"" width "\">"
+       ;; These two <g> tags flip the coordinate system
+       "<g transform=\"translate(0," height ")\">"
+       "<g transform=\"scale(1,-1)\">"
+       (-> (transform width height locations)
+           points
+           line)
+       "</g></g>"
+       "</svg>"))
