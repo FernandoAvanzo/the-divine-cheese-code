3a80dbd560772754f93bae40bb7e4d1d9ae815af
fernando.avanzo <fernando.avanzo@gmail.com>
2022-05-30 12:37:58 -0300

    the-divine-cheese:sample-code: the main funtion that draw a lambda

--- src/the_divine_cheese_code/core.clj
+++ src/the_divine_cheese_code/core.clj
@@ -1,6 +1,8 @@
 (ns the-divine-cheese-code.core
-  (:gen-class)
-  (:require [the-divine-cheese-code.visualization.svg :as svg]))
+  (:require
+    [clojure.java.browse :as browse]
+    [the-divine-cheese-code.visualization.svg :refer [xml]])
+  (:gen-class))
 
 
 (def heists [
@@ -25,6 +27,23 @@
               :lat         41.90
               :lng         12.45}])
 
+(defn url
+  [filename]
+  (str "file:///"
+       (System/getProperty "user.dir")
+       "/"
+       filename))
+
+(defn template
+  [contents]
+  (str "<style>polyline { fill:none; stroke:#5881d8; stroke-width:3}</style>"
+       contents))
+
 (defn -main
   [& _]
-  (println (svg/points heists)))
+  (let [filename "map.html"]
+    (->> heists
+         (xml 50 100)
+         template
+         (spit filename))
+    (browse/browse-url (url filename))))
