71853c15fec3efc6ec75bf152dce97728775d3f0
fernando.avanzo <fernando.avanzo@gmail.com>
2022-07-05 10:30:33 -0300

    the-divine-cheese:sample-code: update clojure version

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject the-divine-cheese-code "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Exercise from the chapter 06 of the Book Clojure for the Brave and True"
   :url "https://github.com/FernandoAvanzo/the-divine-cheese-code"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
